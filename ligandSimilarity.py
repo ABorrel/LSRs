bd2aa5e039dd880d88badfa756a63ff1f19a2789
Alexandre Borrel <a.borrel@gmail.com>
2016-10-29 14:23:13 +0200

    Matrice similarity -> in process

--- ligandSimilarity.py
+++ ligandSimilarity.py
@@ -3,6 +3,8 @@ from shutil import copyfile
 
 import pathManage
 import generateMCS
+import runOtherSoft
+import parseShaep
 
 def analyseLGDProximity(prclassif):
 
@@ -59,30 +61,56 @@ def extractLGDfile(prclassif, prresult):
 
 
 
-def buildMatrixSimilarity(prin, MCS=1, Shaep=1, pfileaffinity=1):
+def buildMatrixSimilarity(prin, MCS=1, Sheap=1, pfileaffinity=1):
 
     lrefprot = listdir(prin)
     for refprot in lrefprot:
         print(refprot)
         lpsmile = []
+        lppdb = []
         lfileref = listdir(prin + refprot)
         # extract smile
         for fileref in lfileref:
             print(fileref[-3:])
             if fileref[-3:] == "smi":
                 lpsmile.append(prin + refprot + "/" + fileref)
+            elif fileref[-3:] == "pdb":
+                lppdb.append(prin + refprot + "/" + fileref)
 
+        # test same number of pdb files and smi
+        if len(lppdb) != len(lpsmile):
+            print("Error -> l.80 ligandSimilarity.py")
+            return
+
+        dresult={}
         i = 0
         nbsmile = len(lpsmile)
         while i < nbsmile - 1:
             j = i + 1
             while j < nbsmile:
-                tanimotoMCS = generateMCS.get_Tanimoto(lpsmile[i], lpsmile[j])
-                print(tanimotoMCS)
+                name1 = lpsmile[i][:-4]
+                name2 = lpsmile[j][:-4]
+                print(name1, name2)
+                if not name1 in dresult.keys():
+                    dresult[name1] == {}
+                if not name2 in dresult[name1].keys():
+                    dresult[name1][name2] = {}
+                if MCS == 1:
+                    tanimotoMCS = generateMCS.get_Tanimoto(lpsmile[i], lpsmile[j])
+                    dresult[name1][name2]["MCS"] = tanimotoMCS
+                    print(tanimotoMCS)
+                if Sheap == 1:
+                    pshaep = prin + refprot + "/outsheap.txt"
+                    runOtherSoft.runShaep(lppdb[i], lppdb[j], pshaep)
+                    dsheap = parseShaep.parseOutputShaep(pshaep)
+                    print(dsheap)
+
+                    runOtherSoft.runShaep(lppdb[j], lppdb[i], pshaep, clean=1)
+                    dsheap = parseShaep.parseOutputShaep(pshaep)
+                    print(dsheap)
+                    dddd
                 # stock
                 j = j + 1
             i = i + 1
-
-
 
     return
--- parseShaep.py
+++ parseShaep.py
@@ -1,39 +1,36 @@
 from os import path
 
 
-def parseOutputShaep (p_filin):
+def parseOutputShaep(p_filin):
     d_out = {}
     
-    if path.exists(p_filin) : 
-        if path.getsize(p_filin) == 0 : 
+    if path.exists(p_filin):
+        if path.getsize(p_filin) == 0:
             return d_out
     
     filin = open (p_filin, "r")
     
-    l_lines = filin.readlines ()
-    filin.close ()
-    if len (l_lines) < 2 : 
+    l_lines = filin.readlines()
+    filin.close()
+    if len(l_lines) < 2:
         return d_out
     
-    l_desc = l_lines[0].strip ().split ("\t")[1:]
-    l_val = l_lines[1].strip ().split ("\t")[1:]
+    l_desc = l_lines[0].strip().split("\t")[1:]
+    l_val = l_lines[1].strip().split("\t")[1:]
     
     i = 0
     nb_desc = 3
-    while i < nb_desc : 
+    while i < nb_desc:
         d_out[l_desc[i]] = l_val[i]
-        i = i + 1
+        i=i + 1
         
     return d_out
 
 
 
 
-def valueShapeSimilarity (p_filin) : 
+def valueShapeSimilarity (p_filin):
     dic = parseOutputShaep (p_filin)
-    if not "shape_similarity" in dic.keys () : 
+    if not "shape_similarity" in dic.keys():
         return 0.0
-    return float (dic["shape_similarity"])
-    
-    
-    
+    return float(dic["shape_similarity"])
